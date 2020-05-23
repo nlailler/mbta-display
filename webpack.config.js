a8245a3ce0ce4c7b70f0a384437bd0b96e02a4a1
Nicholas Lailler <nicholas@lailler.org>
2020-05-23 12:39:28 -0400

    fix dependencies/config

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const webpack = require("webpack");
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
-  entry: "./src/index.js",
+  entry: ["babel-polyfill", "./src/index.js"],
   mode: "development",
   devtool: 'source-map',
   module: {
